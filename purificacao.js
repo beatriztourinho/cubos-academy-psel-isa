cba2a7d23e30978c87159d0ab03cf5c9eab67352
Beatriz <btourinhon@gmail.com>
2021-03-10 21:01:47 -0300

    Solução do Desafio Purificação

---
+++ purificacao.js
@@ -0,0 +1,13 @@
+function solucao(stringCorrompida) {
+    stringCorrompida = stringCorrompida.replace('!', '');
+    stringCorrompida = stringCorrompida.replace('@', '');
+    stringCorrompida = stringCorrompida.replace('#', '');
+    stringCorrompida = stringCorrompida.replace('$', '');
+    stringCorrompida = stringCorrompida.replace('%', '');
+    stringCorrompida = stringCorrompida.replace('&', '');
+    stringCorrompida = stringCorrompida.replace('*', '');
+    stringCorrompida = stringCorrompida.replace('(', '');
+    stringCorrompida = stringCorrompida.replace(')', '');
+    
+    console.log(stringCorrompida)
+}
